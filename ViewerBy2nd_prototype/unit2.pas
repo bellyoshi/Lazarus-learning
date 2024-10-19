1e6e0b1b1577015bb72634f6cf81efbfba6c246e
bellyoshi <tccwas@gmail.com>
2024-10-19 18:17:09 +0900

    #

--- ViewerBy2nd_prototype/unit2.pas
+++ ViewerBy2nd_prototype/unit2.pas
@@ -5,7 +5,7 @@ unit Unit2;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Menus;
 
 type
 
@@ -13,13 +13,26 @@ type
 
   TForm2 = class(TForm)
     Image1: TImage;
+    MenuItemFullScreen: TMenuItem;
+    MenuItemWindowMode: TMenuItem;
+    MenuItemClose: TMenuItem;
+    PopupMenu1: TPopupMenu;
+
+
+
     procedure FormCreate(Sender: TObject);
     procedure FormResize(Sender: TObject);
-    procedure Image1Click(Sender: TObject);
-  private
 
+    procedure FormContextPopup(Sender: TObject; MousePos: TPoint; var Handled: Boolean);
+    procedure MenuItemCloseClick(Sender: TObject);
+    procedure MenuItemFullScreenClick(Sender: TObject);
+    procedure MenuItemWindowModeClick(Sender: TObject);
+  private
+    FIsFullScreen: Boolean;
+    procedure SetIsFullScreen(Value: Boolean);
   public
     procedure SetBitmap(Bitmap: TBitmap);
+    property IsFullScreen: Boolean read FIsFullScreen write SetIsFullScreen;
 
   end;
 
@@ -44,10 +57,49 @@ begin
   Image1.Height := ClientHeight;
 end;
 
-procedure TForm2.Image1Click(Sender: TObject);
-begin
 
+
+procedure TForm2.SetIsFullScreen(Value: Boolean);
+begin
+  if FIsFullScreen = Value then Exit; // 既にフルスクリーン状態なら何もしない
+
+  FIsFullScreen := Value;
+
+  if FIsFullScreen then
+  begin
+    // フルスクリーンモードに切り替える
+    BorderStyle := bsNone;
+    WindowState := wsMaximized;
+  end
+  else
+  begin
+    // ウィンドウモードに戻す
+    BorderStyle := bsSizeable;
+    WindowState := wsNormal;
+  end;
 end;
+
+procedure TForm2.FormContextPopup(Sender: TObject; MousePos: TPoint; var Handled: Boolean);
+begin
+  PopupMenu1.PopUp(Mouse.CursorPos.X, Mouse.CursorPos.Y);
+  Handled := True;  // イベントを処理済みにする
+end;
+
+procedure TForm2.MenuItemFullScreenClick(Sender: TObject);
+begin
+  IsFullScreen := True;  // フルスクリーンに切り替える
+end;
+
+procedure TForm2.MenuItemWindowModeClick(Sender: TObject);
+begin
+  IsFullScreen := False;  // ウインドウモードに切り替える
+end;
+
+procedure TForm2.MenuItemCloseClick(Sender: TObject);
+begin
+  Close;  // フォームを閉じる
+end;
+
 
 
 procedure TForm2.SetBitmap(Bitmap : TBitmap);

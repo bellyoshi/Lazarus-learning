52712f9ca415cd1203ba4ed91b8985661e13b92c
bellyoshi <tccwas@gmail.com>
2025-06-05 17:04:20 +0900

    #

--- ViewerBy2nd_prototype/viewerby2ndfiletypes.pas
+++ ViewerBy2nd_prototype/viewerby2ndfiletypes.pas
@@ -7,9 +7,15 @@ interface
 uses
   Classes, SysUtils, FPImage, FPReadJPEG, FPReadPNG, FPReadBMP;
 
+const
+  IMAGE_EXTENSIONS: array[0..3] of string = ('.jpg', '.jpeg', '.png', '.bmp');
+  PDF_EXTENSIONS: array[0..0] of string = ('.pdf');
+  MOVIE_EXTENSIONS: array[0..3] of string = ('.mp4', '.avi', '.mov', '.wmv');
+
 function CanOpen(const FileName: string): Boolean;
 function IsImageFile(const FileName: string): Boolean;
 function IsPDFFile(const FileName: string): Boolean;
+function IsMovie(const FileName: string): Boolean;
 function GetFileFilter: string;
 function GetTFPReader(const FileName: string): TFPCustomImageReader;
 
@@ -18,27 +24,84 @@ implementation
 function IsImageFile(const FileName: string): Boolean;
 var
   Ext: string;
+  i: Integer;
 begin
   Ext := LowerCase(ExtractFileExt(FileName));
-  Result := (Ext = '.jpg') or (Ext = '.jpeg') or (Ext = '.png') or (Ext = '.bmp');
+  Result := False;
+  for i := Low(IMAGE_EXTENSIONS) to High(IMAGE_EXTENSIONS) do
+  begin
+    if Ext = IMAGE_EXTENSIONS[i] then
+    begin
+      Result := True;
+      Exit;
+    end;
+  end;
 end;
 
 function IsPDFFile(const FileName: string): Boolean;
 var
   Ext: string;
+  i: Integer;
 begin
   Ext := LowerCase(ExtractFileExt(FileName));
-  Result := Ext = '.pdf';
+  Result := False;
+  for i := Low(PDF_EXTENSIONS) to High(PDF_EXTENSIONS) do
+  begin
+    if Ext = PDF_EXTENSIONS[i] then
+    begin
+      Result := True;
+      Exit;
+    end;
+  end;
+end;
+
+function IsMovie(const FileName: string): Boolean;
+var
+  Ext: string;
+  i: Integer;
+begin
+  Ext := LowerCase(ExtractFileExt(FileName));
+  Result := False;
+  for i := Low(MOVIE_EXTENSIONS) to High(MOVIE_EXTENSIONS) do
+  begin
+    if Ext = MOVIE_EXTENSIONS[i] then
+    begin
+      Result := True;
+      Exit;
+    end;
+  end;
 end;
 
 function CanOpen(const FileName: string): Boolean;
 begin
-  Result := IsImageFile(FileName) or IsPDFFile(FileName);
+  Result := IsImageFile(FileName) or IsPDFFile(FileName) or IsMovie(FileName);
 end;
 
 function GetFileFilter: string;
+var
+  ImageFilter, MovieFilter: string;
+  i: Integer;
 begin
-  Result := 'PDF Files|*.pdf|Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*';
+  // Build image filter
+  ImageFilter := '';
+  for i := Low(IMAGE_EXTENSIONS) to High(IMAGE_EXTENSIONS) do
+  begin
+    if i > Low(IMAGE_EXTENSIONS) then
+      ImageFilter := ImageFilter + ';';
+    ImageFilter := ImageFilter + '*' + IMAGE_EXTENSIONS[i];
+  end;
+
+  // Build movie filter
+  MovieFilter := '';
+  for i := Low(MOVIE_EXTENSIONS) to High(MOVIE_EXTENSIONS) do
+  begin
+    if i > Low(MOVIE_EXTENSIONS) then
+      MovieFilter := MovieFilter + ';';
+    MovieFilter := MovieFilter + '*' + MOVIE_EXTENSIONS[i];
+  end;
+
+  Result := Format('PDF Files|*.pdf|Image Files|%s|Movie Files|%s|All Files|*.*',
+    [ImageFilter, MovieFilter]);
 end;
 
 function GetTFPReader(const FileName: string): TFPCustomImageReader;
